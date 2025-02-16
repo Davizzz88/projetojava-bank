327f7e311f069aa9227efb58297f7510eb71fd42
Junior <junior.jr.jl@gmail.com>
2025-02-16 15:47:02 -0300

    aula 2 criação dos repositories

---
+++ src/main/java/br/com/dio/expcetion/NoFundsEnoughException.java
@@ -0,0 +1,9 @@
+package br.com.dio.expcetion;
+
+public class NoFundsEnoughException extends RuntimeException {
+
+    public NoFundsEnoughException(String message) {
+        super(message);
+    }
+
+}
